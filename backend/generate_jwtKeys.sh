054877c5a77827eb39f274212a4eb7efd5d13039
it180171 <david.altenhofer@gmail.com>
2023-02-13 13:23:35 +0100

    #51 Problem mit JWT signing im backend gelöst, Änderungen am .sh File, Speicherort der Tokens geändert

--- backend/generate_jwtKeys.sh
+++ backend/generate_jwtKeys.sh
@@ -1,6 +1,6 @@
 mkdir jwt
-openssl genrsa -out jwt/rsaPrivateKey.pem 2048
-openssl rsa -pubout -in jwt/rsaPrivateKey.pem -out jwt/publicKey.pem
-openssl pkcs8 -topk8 -nocrypt -inform pem -in jwt/rsaPrivateKey.pem -outform pem -out jwt/privateKey.pem
-chmod 600 jwt/rsaPrivateKey.pem
-chmod 600 jwt/privateKey.pem
+openssl genrsa -out src/main/resources/pk/rsaPrivateKey.pem 2048
+openssl rsa -pubout -in src/main/resources/pk/rsaPrivateKey.pem -out src/main/resources/pk/publicKey.pem
+openssl pkcs8 -topk8 -nocrypt -inform pem -in src/main/resources/pk/rsaPrivateKey.pem -outform pem -out src/main/resources/pk/privateKey.pem
+chmod 600 src/main/resources/pk/rsaPrivateKey.pem
+chmod 600 src/main/resources/pk/privateKey.pem
